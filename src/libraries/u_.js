dfa5bbebea13f4587a7b70350ce70c7a3797b621
hys.pm <khayashi@dip.co.jp>
2022-01-20 17:35:13 +0900

    modify: [libraries] module aliasの使用を取りやめ

--- src/libraries/u_.js
+++ src/libraries/u_.js
@@ -1,11 +1,11 @@
-import "@@assets/scripts/u_/RegExp.escape";
-import it from "@@assets/scripts/u_/isType";
-import utill from "@@assets/scripts/u_/utill";
-import mb from "@@assets/scripts/u_/mb";
-import qs from "@@assets/scripts/u_/qs";
-import fileInfo from "@@assets/scripts/u_/fileInfo";
-import rfc from "@@assets/scripts/u_/rfc";
-import mapRecursive from "@@assets/scripts/u_/mapRecursive";
+import "../assets/scripts/u_/RegExp.escape";
+import it from "../assets/scripts/u_/isType";
+import utill from "../assets/scripts/u_/utill";
+import mb from "../assets/scripts/u_/mb";
+import qs from "../assets/scripts/u_/qs";
+import fileInfo from "../assets/scripts/u_/fileInfo";
+import rfc from "../assets/scripts/u_/rfc";
+import mapRecursive from "../assets/scripts/u_/mapRecursive";
 
 export default {
 	_version: PACKAGE_VERSION,
--- src/libraries/wu_.js
+++ src/libraries/wu_.js
@@ -1,8 +1,8 @@
-import attachEventListener from "@@assets/scripts/wu_/attachEventListener";
-import attachHoverStatus from "@@assets/scripts/wu_/attachHoverStatus";
-import browserInfo from "@@assets/scripts/wu_/browserInfo";
-import deviceInfo from "@@assets/scripts/wu_/deviceInfo";
-import onDocumentReady from "@@assets/scripts/wu_/onDocumentReady";
+import attachEventListener from "../assets/scripts/wu_/attachEventListener";
+import attachHoverStatus from "../assets/scripts/wu_/attachHoverStatus";
+import browserInfo from "../assets/scripts/wu_/browserInfo";
+import deviceInfo from "../assets/scripts/wu_/deviceInfo";
+import onDocumentReady from "../assets/scripts/wu_/onDocumentReady";
 
 export default {
 	_version: PACKAGE_VERSION,
